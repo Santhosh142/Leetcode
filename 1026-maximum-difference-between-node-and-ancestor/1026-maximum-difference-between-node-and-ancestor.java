f01c309e22d5b8c38e2b4d2479c3e5c919ee43d2
Santhosh142 <77147724+Santhosh142@users.noreply.github.com>
2022-12-09 12:51:06 +0530

    Time: 1 ms (80.02%), Space: 42.5 MB (43.30%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
@@ -16,22 +16,21 @@
 class Solution {
     int maxi = Integer.MIN_VALUE;
     public int maxAncestorDiff(TreeNode root) {
-        utility(root);
+        solve(root, root.val, root.val);
         return maxi;
     }
-    public void getMax(TreeNode node, TreeNode check) {
-        if(node == null || check == null) return;
-        int diff = Math.abs(node.val - check.val);
-        maxi = Math.max(diff, maxi);
-        getMax(node, check.left);
-        getMax(node, check.right);
+    
+    public void solve(TreeNode root, int minAncestor, int maxAncestor) {
+        if(root == null) return;
         
-    }
-    public void utility(TreeNode node) {
-        if(node == null) return;
-        getMax(node, node);
-        utility(node.left);
-        utility(node.right);
+        int maxDiff = Math.max(Math.abs(root.val - minAncestor), Math.abs(root.val - maxAncestor));
+        
+        maxi = Math.max(maxi, maxDiff);
+        if(root.val > maxAncestor) maxAncestor = root.val;
+        if(root.val < minAncestor) minAncestor = root.val;
+        solve(root.left, minAncestor, maxAncestor);
+        solve(root.right, minAncestor, maxAncestor);
+
     }
     
 
