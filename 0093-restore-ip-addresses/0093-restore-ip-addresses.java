2f632896dccedf59b0e3db67449856e8d9f5b47c
Santhosh142 <77147724+Santhosh142@users.noreply.github.com>
2023-01-21 19:49:35 +0530

    Time: 9 ms (37.65%), Space: 42.6 MB (67.31%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -1,23 +1,24 @@
 class Solution {
-    
+    int len;
     public List<String> restoreIpAddresses(String s) {
         List<String> res = new ArrayList<>();
+        len = s.length();
         solve(s, 0, 0, "", res);
         return res;
     }
     public void solve(String s, int i, int part, String ans, List<String> res) {
-        if(i == s.length() || part == 4) {
-            if(i == s.length() && part == 4) {
+        if(i == len || part == 4) {
+            if(i == len && part == 4) {
                 res.add(ans.substring(0, ans.length() - 1));
             }
             return;
         }
         solve(s, i + 1, part + 1, ans + s.charAt(i) + ".", res);
         
-        if(i + 2 <= s.length() && isValid(s.substring(i,i + 2)))
+        if(i + 2 <= len && isValid(s.substring(i,i + 2)))
             solve(s, i + 2, part + 1, ans + s.substring(i,i + 2) + ".", res);
         
-        if(i + 3 <= s.length() && isValid(s.substring(i,i + 3)))
+        if(i + 3 <= len && isValid(s.substring(i,i + 3)))
             solve(s, i + 3, part + 1, ans + s.substring(i,i + 3) + ".", res);
     }
     public boolean isValid(String s) {
