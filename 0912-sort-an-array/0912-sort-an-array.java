888f135c8fff45581320403bc686e9949225303f
Santhosh142 <77147724+Santhosh142@users.noreply.github.com>
2023-03-01 16:09:25 +0530

    Time: 34 ms (62.37%), Space: 53.7 MB (42.42%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,6 +1,35 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
+        mergesort(nums, 0, nums.length - 1);
         return nums;
+    }
+    public void mergesort(int[] nums, int low, int high) {
+        if(low < high) {
+            int mid = low + (high - low) / 2;
+            mergesort(nums, low, mid);
+            mergesort(nums, mid + 1, high);
+            conquer(nums, low , mid , high);
+        }
+    }
+    public void conquer(int[] nums, int low, int mid, int high) {
+        int n = high - low + 1;
+        int[] merged = new int[n];
+        int i = low, j = mid + 1, ind = 0;
+        while(i <= mid && j <= high) {
+            if(nums[i] < nums[j]) {
+                merged[ind++] = nums[i++];
+            } else {
+                merged[ind++] = nums[j++];
+            }
+        }
+        while(i <= mid) {
+            merged[ind++] = nums[i++];
+        }
+        while(j <= high) {
+            merged[ind++] = nums[j++];
+        }
+        for(i = low, j = 0; j < n; i++, j++) {
+            nums[i] = merged[j];
+        }
     }
 }
