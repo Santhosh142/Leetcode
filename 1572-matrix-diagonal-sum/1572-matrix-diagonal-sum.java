005888e4d55a4cd18fec058d2f345ee10e1b8913
Santhosh142 <77147724+Santhosh142@users.noreply.github.com>
2023-05-08 15:47:41 +0530

    Time: 1 ms (30.60%), Space: 42.8 MB (45.49%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -5,9 +5,7 @@ class Solution {
         
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
-                if(i == j) {
-                    sum += mat[i][j];
-                } else if((i + j) == n - 1) {
+                if(i == j || (i + j) == n - 1) {
                     sum += mat[i][j];
                 }
             }
