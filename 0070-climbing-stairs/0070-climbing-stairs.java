c91ee19145c6656b8fd96ef91f96c9c14f8a96b4
Santhosh142 <77147724+Santhosh142@users.noreply.github.com>
2023-06-28 14:24:18 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (15.40%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,14 +2,11 @@ class Solution {
     public int climbStairs(int n) {
         int[] dp = new int[n + 1];
         Arrays.fill(dp, 0);
-        return memoize(dp, n);
-    }
-    
-    public int memoize(int[] dp, int n) {
-        if(n == 0 || n == 1) return 1;
-        
-        if(dp[n] != 0) return dp[n];
-        
-        return dp[n] = memoize(dp, n - 1) + memoize(dp, n - 2);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
