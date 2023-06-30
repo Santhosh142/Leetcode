78cfaf20d79db4c57b0b75eded2db0352e6dc4bc
Santhosh142 <77147724+Santhosh142@users.noreply.github.com>
2023-06-30 10:23:25 +0530

    Time: 1 ms (99.77%), Space: 44.2 MB (48.06%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,26 +1,24 @@
 class Solution {
-    public int minPathSum(int[][] grid) 
-    {
-        int rows = grid.length;            
-        int cols = grid[0].length;
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n  = grid[0].length;
+        int[][] dp = new int[m][n];
+        for(int[] ar : dp) Arrays.fill(ar, -1 );
         
-        int[][] dp = new int[rows][cols];
-        for(int i=0;i<rows;i++) for(int j=0;j<cols;j++) dp[i][j] = -1;
-        return solve(grid,rows-1,cols-1,dp);
-    } 
-    public int solve(int[][] grid,int rows,int cols,int[][] dp)
-    {
-        if(rows < 0 || cols < 0)
-            return Integer.MAX_VALUE;
-        if(rows == 0 && cols ==0)
-            return grid[rows][cols];
-        if(dp[rows][cols] != -1)
-            return dp[rows][cols];
-        
-        int up = solve(grid,rows-1,cols,dp);
-        int left = solve(grid,rows,cols-1,dp);
-        
-        return dp[rows][cols] = grid[rows][cols] + Math.min(up,left);
+        int res = memoize(grid, dp, m - 1, n - 1);
+        return res;
     }
     
+    public int memoize(int[][] grid, int[][] dp, int i, int j) {
+        if(i < 0 || j < 0)
+            return Integer.MAX_VALUE;
+        if(i == 0 && j == 0) return grid[i][j];
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int left = memoize(grid, dp, i , j - 1);
+        int up = memoize(grid, dp, i - 1, j);
+        
+        return dp[i][j] = grid[i][j] + Math.min(up, left);
+    }
 }
